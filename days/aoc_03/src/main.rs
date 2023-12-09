11f6fcbd20bc589670596cd67a5eac8c77c26c82
Mathias Kindberg <mathias@kindberg.earth>
2023-12-09 14:43:08 +0100

    day 3 part 2 done

--- days/aoc_03/src/main.rs
+++ days/aoc_03/src/main.rs
@@ -1,7 +1,7 @@
 //! One: 525911
-//! Two:
+//! Two: 75805607
 
-use std::io::BufRead;
+use std::{io::BufRead, vec};
 
 fn input() -> Vec<Vec<char>> {
     let stdin = std::io::stdin();
@@ -30,18 +30,20 @@ fn pad_input(mut input: Vec<Vec<char>>) -> Vec<Vec<char>> {
     input
 }
 
-/// `...`
-/// `.X.`
-/// `...`
-fn adjacent_symbol(x: usize, y: usize, input: &[Vec<char>]) -> bool {
-    for row in input.iter().skip(y - 1).take(3) {
-        for symbol in row.iter().skip(x - 1).take(3) {
-            if !symbol.is_ascii_digit() && symbol != &'.' {
-                return true;
+fn find_adjacent_symbol(
+    x: usize,
+    y: usize,
+    input: &[Vec<char>],
+    symbol_check: fn(&char) -> bool,
+) -> Option<(usize, usize)> {
+    for (y, row) in input.iter().enumerate().skip(y - 1).take(3) {
+        for (x, symbol) in row.iter().enumerate().skip(x - 1).take(3) {
+            if symbol_check(symbol) {
+                return Some((x, y));
             }
         }
     }
-    false
+    None
 }
 
 fn one(input: &[Vec<char>]) {
@@ -57,11 +59,17 @@ fn one(input: &[Vec<char>]) {
 
         for (x, char) in row[1..(row.len())].iter().enumerate() {
             if char.is_ascii_digit() {
-                if !has_adjacent_symbol && adjacent_symbol(x + PADDING, y + PADDING, &input) {
+                if !has_adjacent_symbol
+                    && find_adjacent_symbol(x + PADDING, y + PADDING, &input, |symbol: &char| {
+                        {
+                            !symbol.is_ascii_digit() && symbol != &'.'
+                        }
+                    })
+                    .is_some()
+                {
                     has_adjacent_symbol = true
                 }
-                let digit: u64 = char.to_digit(10).unwrap().into();
-                num = num * 10 + digit;
+                num = num * 10 + u64::from(char.to_digit(10).unwrap());
             } else {
                 if has_adjacent_symbol {
                     sum += num;
@@ -75,9 +83,55 @@ fn one(input: &[Vec<char>]) {
 
     println!("One: {sum} | Elapsed: {:?}", now.elapsed());
 }
-fn two(_input: &[Vec<char>]) {
+
+fn two(input: &[Vec<char>]) {
+    const PADDING: usize = 1;
+
     let now = std::time::Instant::now();
-    let sum = 0;
+    let mut gears = vec![vec![vec![]; input[0].len()]; input.len()];
+
+    let input = pad_input(input.to_owned());
+
+    for (y, row) in input[1..(input.len() - 1)].iter().enumerate() {
+        let mut num: u64 = 0;
+        let _has_adjacent_gear = false;
+        let mut adjacent_gear = None;
+
+        for (x, char) in row[1..(row.len())].iter().enumerate() {
+            if char.is_ascii_digit() {
+                if let Some((x, y)) =
+                    find_adjacent_symbol(x + PADDING, y + PADDING, &input, |symbol: &char| {
+                        symbol == &'*'
+                    })
+                {
+                    adjacent_gear = Some((x, y));
+                }
+                num = num * 10 + u64::from(char.to_digit(10).unwrap());
+            } else {
+                if let Some((x, y)) = adjacent_gear {
+                    gears[y][x].push(num)
+                }
+
+                adjacent_gear = None;
+                num = 0;
+            }
+        }
+    }
+
+    let sum: u64 = gears
+        .iter()
+        .flat_map(|gears| {
+            gears.iter().filter(|gear| gear.len() == 2).map(|gears| {
+                gears
+                    .iter()
+                    .fold(None, |acc, val| match acc {
+                        Some(acc) => Some(acc * val),
+                        None => Some(*val),
+                    })
+                    .unwrap()
+            })
+        })
+        .sum();
 
     println!("Two: {sum} | Elapsed: {:?}", now.elapsed());
 }
