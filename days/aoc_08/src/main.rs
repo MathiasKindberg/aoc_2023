b67f437301724cef47f436c37ee7d73f3cb35d24
Mathias Kindberg <mathias@kindberg.earth>
2023-12-13 19:28:17 +0100

    Most parsing for day 8 done.

--- days/aoc_08/src/main.rs
+++ days/aoc_08/src/main.rs
@@ -1,16 +1,54 @@
 //! Part 1:
 //! Part 2:
 
-use std::io::BufRead;
+use std::{collections::HashMap, io::BufRead};
 
 fn input() -> Vec<String> {
     let stdin = std::io::stdin();
     stdin.lock().lines().map_while(Result::ok).collect()
 }
 
-fn one(_input: &[String]) {
+#[derive(Debug)]
+struct Destination {
+    left: String,
+    right: String,
+}
+
+fn one(input: &[String]) {
+    use itertools::Itertools;
+
     let now = std::time::Instant::now();
     let sum = 0;
+
+    println!("{input:?}");
+
+    // let movement = input[0].
+
+    let input: HashMap<_, _> = input[2..]
+        .iter()
+        .map(|row| row.split(" = ").collect_tuple().unwrap())
+        .map(|(source, destination)| {
+            let cleaned_input = destination
+                .replace('(', "")
+                .replace(')', "")
+                .replace(",", "");
+            let (left, right) = cleaned_input
+                .split_ascii_whitespace()
+                .collect_tuple()
+                .unwrap();
+            (
+                source.to_owned(),
+                Destination {
+                    left: left.to_owned(),
+                    right: right.to_owned(),
+                },
+            )
+        })
+        .collect();
+
+    for row in input {
+        println!("{row:?}");
+    }
 
     println!("One: {sum} | Elapsed: {:?}", now.elapsed());
 }
