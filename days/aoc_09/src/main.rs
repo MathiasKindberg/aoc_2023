9de94ec23dc5811b628342a51fdfa069cbe3e28c
Mathias Kindberg <mathias@kindberg.earth>
2023-12-15 18:14:03 +0100

    day 9 part 2 done

--- days/aoc_09/src/main.rs
+++ days/aoc_09/src/main.rs
@@ -1,19 +1,18 @@
 //! Part 1: 1898776583
-//! Part 2:
+//! Part 2: 1100
 
-use std::io::BufRead;
+use ::lending_iterator::prelude::*;
+use std::{collections::VecDeque, io::BufRead};
 
 fn input() -> Vec<String> {
     let stdin = std::io::stdin();
     stdin.lock().lines().map_while(Result::ok).collect()
 }
 
-fn calculate_differences(row: Vec<i64>) -> Vec<i64> {
+fn calculate_differences(row: &[i64]) -> Vec<i64> {
     use itertools::Itertools;
-    row.into_iter()
-        .tuple_windows()
-        .map(|(a, b)| b - a)
-        .collect()
+
+    row.iter().tuple_windows().map(|(a, b)| b - a).collect()
 }
 
 fn one(input: &[String]) {
@@ -33,7 +32,7 @@ fn one(input: &[String]) {
         let mut current_row = row[0].clone();
         // Could do this recursively, hmmm!
         loop {
-            current_row = calculate_differences(current_row);
+            current_row = calculate_differences(&current_row);
             if current_row.iter().sum::<i64>() == 0 {
                 break;
             } else {
@@ -45,9 +44,49 @@ fn one(input: &[String]) {
 
     println!("One: {sum} | Elapsed: {:?}", now.elapsed());
 }
-fn two(_input: &[String]) {
+
+fn calculate_differences_vecdeque(row: &VecDeque<i64>) -> VecDeque<i64> {
+    use itertools::Itertools;
+
+    row.iter().tuple_windows().map(|(a, b)| b - a).collect()
+}
+
+fn two(input: &[String]) {
     let now = std::time::Instant::now();
-    let sum = 0;
+    let mut sum = 0;
+
+    let mut input: Vec<Vec<_>> = input
+        .iter()
+        .map(|row| {
+            vec![row
+                .split_ascii_whitespace()
+                .map(|value| value.parse::<i64>().expect("valid integer"))
+                // VecDeque to enable fast inserting from beginning of vec
+                .collect::<VecDeque<_>>()]
+        })
+        .collect();
+
+    for row in &mut input {
+        let mut current_row = row[0].clone();
+        loop {
+            current_row = calculate_differences_vecdeque(&current_row);
+            row.push(current_row.clone());
+            if current_row.iter().sum::<i64>() == 0 {
+                break;
+            }
+        }
+
+        // Start from the bottom.
+        row.reverse();
+
+        // One day we will have a true nice windows_mut and lending iterator in std.
+        let mut iter = row.windows_mut::<2>();
+        while let Some([ref mut curr, ref mut last]) = iter.next() {
+            last.push_front(last.front().unwrap() - curr.front().unwrap());
+        }
+
+        sum += row.last().unwrap().front().unwrap();
+    }
 
     println!("Two: {sum} | Elapsed: {:?}", now.elapsed());
 }
