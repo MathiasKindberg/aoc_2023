eddf0862a725f7323220a222eb22cea665fde2e7
Mathias Kindberg <mathias@kindberg.earth>
2023-12-15 16:54:44 +0100

    day 9 part one done

--- days/aoc_09/src/main.rs
+++ days/aoc_09/src/main.rs
@@ -1,4 +1,4 @@
-//! Part 1:
+//! Part 1: 1898776583
 //! Part 2:
 
 use std::io::BufRead;
@@ -8,9 +8,40 @@ fn input() -> Vec<String> {
     stdin.lock().lines().map_while(Result::ok).collect()
 }
 
-fn one(_input: &[String]) {
+fn calculate_differences(row: Vec<i64>) -> Vec<i64> {
+    use itertools::Itertools;
+    row.into_iter()
+        .tuple_windows()
+        .map(|(a, b)| b - a)
+        .collect()
+}
+
+fn one(input: &[String]) {
     let now = std::time::Instant::now();
-    let sum = 0;
+    let mut sum: i64 = 0;
+    let mut input: Vec<Vec<_>> = input
+        .iter()
+        .map(|row| {
+            vec![row
+                .split_ascii_whitespace()
+                .map(|value| value.parse::<i64>().expect("valid integer"))
+                .collect::<Vec<_>>()]
+        })
+        .collect();
+
+    for row in &mut input {
+        let mut current_row = row[0].clone();
+        // Could do this recursively, hmmm!
+        loop {
+            current_row = calculate_differences(current_row);
+            if current_row.iter().sum::<i64>() == 0 {
+                break;
+            } else {
+                row.push(current_row.clone())
+            }
+        }
+        sum += row.iter().map(|diffs| diffs.last().unwrap()).sum::<i64>();
+    }
 
     println!("One: {sum} | Elapsed: {:?}", now.elapsed());
 }
