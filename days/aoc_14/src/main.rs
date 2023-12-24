0ba6cbc796e29ca77a030aec1c1d2569c8d2b945
Mathias Kindberg <mathias@kindberg.earth>
2023-12-25 00:54:04 +0100

    Day 14 part 2 done

--- days/aoc_14/src/main.rs
+++ days/aoc_14/src/main.rs
@@ -1,9 +1,9 @@
-//! Part 1:
-//! Part 2:
+//! Part 1: 110779
+//! Part 2: 86069
 
 use std::io::BufRead;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Copy, Hash)]
 enum Ground {
     Round,
     Cube,
@@ -39,26 +39,111 @@ fn input() -> Vec<Vec<Ground>> {
         .collect()
 }
 
+fn tilt_west(input: &mut Vec<Vec<Ground>>) {
+    for row in input {
+        // Compact row
+        // Iterate by char, if empty grab next movable char and move next to it.
+        // Could make it more efficient by moving groups of chars.
+        for idx in 0..row.len() {
+            match row[idx] {
+                // Find next empty and move it here
+                Ground::Empty => {
+                    for movable_idx in idx + 1..row.len() {
+                        let ground_type = &row[movable_idx];
+                        match ground_type {
+                            // Can't move a cube
+                            Ground::Cube => break,
+                            Ground::Round => {
+                                row[idx] = *ground_type;
+                                row[movable_idx] = Ground::Empty;
+
+                                // Ensure that we do not try to move multiple rounds to the same location
+                                break;
+                            }
+                            Ground::Empty => (),
+                        }
+                    }
+                }
+                // Do nothing
+                Ground::Round | Ground::Cube => (),
+            }
+        }
+    }
+}
+
 fn one(input: Vec<Vec<Ground>>) {
     let now = std::time::Instant::now();
-    let sum = 0;
 
-    for row in &input {
-        for c in row {
-            print!("{c}")
-        }
-        println!()
-    }
-
+    // Transpose input so we do not need to juggle columns
+    let mut input = aoc_lib::transpose2(input);
+    tilt_west(&mut input);
     let input = aoc_lib::transpose2(input);
 
-    println!("One: {sum} | Elapsed: {:?}", now.elapsed());
-}
-fn two(_input: Vec<Vec<Ground>>) {
-    let now = std::time::Instant::now();
-    let sum = 0;
+    // Calculate load
+    let total_load: usize = input
+        .iter()
+        .zip((1..input.len() + 1).rev())
+        .map(|(row, load)| {
+            row.iter()
+                .filter(|ground| ground == &&Ground::Round)
+                .count()
+                * load
+        })
+        .sum();
 
-    println!("Two: {sum} | Elapsed: {:?}", now.elapsed());
+    println!("One: {total_load} | Elapsed: {:?}", now.elapsed());
+}
+
+fn cycle(input: &mut Vec<Vec<Ground>>) {
+    for _ in 0..4 {
+        aoc_lib::rotate_90_cw_2d(input);
+        tilt_west(input);
+    }
+}
+
+fn two(mut input: Vec<Vec<Ground>>) {
+    const CYCLES: usize = 1_000_000_000;
+    let now = std::time::Instant::now();
+
+    // -180 degrees means north -> south. Then we rotate clock-wise means what originally
+    // was north -> west. Then we tilt west and start cycling.
+    aoc_lib::rotate_90_ccw_2d(&mut input);
+    aoc_lib::rotate_90_ccw_2d(&mut input);
+
+    let mut seen: Vec<_> = vec![input.clone()];
+    let mut final_grid = None;
+
+    for _ in 0..CYCLES {
+        cycle(&mut input);
+
+        if let Some(idx) = seen.iter().position(|x| x == &input) {
+            let cycle_len = seen.len() - idx;
+            let final_idx = idx + (CYCLES - idx) % cycle_len;
+            final_grid = Some(seen[final_idx].clone());
+            break;
+        }
+
+        seen.push(input.clone());
+    }
+
+    input = final_grid.unwrap();
+
+    // Undo our initial rotation.
+    aoc_lib::rotate_90_ccw_2d(&mut input);
+    aoc_lib::rotate_90_ccw_2d(&mut input);
+
+    let total_load: usize = input
+        .iter()
+        .zip((1..input.len() + 1).rev())
+        .map(|(row, load)| {
+            row.iter()
+                .filter(|ground| ground == &&Ground::Round)
+                .count()
+                * load
+        })
+        .sum();
+
+    println!("Two: {total_load} | Elapsed: {:?}", now.elapsed());
 }
 
 fn main() {
--- days/aoc_lib/src/lib.rs
+++ days/aoc_lib/src/lib.rs
@@ -13,6 +13,74 @@ pub fn transpose2<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
         .collect()
 }
 
+/// Reverses all vectors in 2d Vec
+pub fn rev2<T>(v: &mut Vec<Vec<T>>) {
+    for row in v {
+        row.reverse()
+    }
+}
+
+pub fn rotate_90_ccw_2d<T>(matrix: &mut Vec<Vec<T>>)
+where
+    T: Copy,
+{
+    let size = matrix.len();
+
+    for x in 0..(size / 2) {
+        for y in x..size - x - 1 {
+            let temp = matrix[x][y];
+            matrix[x][y] = matrix[y][size - 1 - x];
+            matrix[y][size - 1 - x] = matrix[size - 1 - x][size - 1 - y];
+            matrix[size - 1 - x][size - 1 - y] = matrix[size - 1 - y][x];
+            matrix[size - 1 - y][x] = temp;
+        }
+    }
+}
+
+pub fn rotate_90_cw_2d<T>(matrix: &mut Vec<Vec<T>>)
+where
+    T: Copy,
+{
+    let size = matrix.len();
+
+    // Traverse each cycle
+    for i in 0..(size / 2) {
+        for j in i..(size - i - 1) {
+            // Swap elements of each cycle
+            // in clockwise direction
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[size - 1 - j][i];
+            matrix[size - 1 - j][i] = matrix[size - 1 - i][size - 1 - j];
+            matrix[size - 1 - i][size - 1 - j] = matrix[j][size - 1 - i];
+            matrix[j][size - 1 - i] = temp;
+        }
+    }
+    // for (int i = 0; i < N / 2; i++) {
+    //     for (int j = i; j < N - i - 1; j++) {
+
+    //         // Swap elements of each cycle
+    //         // in clockwise direction
+    //         int temp = a[i][j];
+    //         a[i][j] = a[N - 1 - j][i];
+    //         a[N - 1 - j][i] = a[N - 1 - i][N - 1 - j];
+    //         a[N - 1 - i][N - 1 - j] = a[j][N - 1 - i];
+    //         a[j][N - 1 - i] = temp;
+    //     }
+    // }
+}
+
+pub fn print_2d<T>(v: &[Vec<T>])
+where
+    T: std::fmt::Display,
+{
+    for row in v {
+        for c in row {
+            print!("{c} ");
+        }
+        println!()
+    }
+}
+
 // /// Two dimensional helper for quick row based and columnar iteration
 // pub struct TwoDVec<T> {
 //     data: Vec<T>,
