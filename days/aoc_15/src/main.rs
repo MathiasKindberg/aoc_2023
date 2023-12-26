7d300fae1deee203dd458c5fc4e8a1bda6351e1b
Mathias Kindberg <mathias@kindberg.earth>
2023-12-26 13:27:55 +0100

    day 15 part 1 done

--- days/aoc_15/src/main.rs
+++ days/aoc_15/src/main.rs
@@ -8,10 +8,20 @@ fn input() -> Vec<String> {
     stdin.lock().lines().map_while(Result::ok).collect()
 }
 
-fn one(_input: &[String]) {
-    let now = std::time::Instant::now();
-    let sum = 0;
+fn hash(mut current_value: u64, c: char) -> u64 {
+    current_value += c as u64;
+    current_value *= 17;
+    current_value %= 256;
+    current_value
+}
 
+fn encode(s: &str) -> u64 {
+    s.chars().fold(0, hash)
+}
+
+fn one(input: &[String]) {
+    let now = std::time::Instant::now();
+    let sum: u64 = input[0].split(',').map(encode).sum();
     println!("One: {sum} | Elapsed: {:?}", now.elapsed());
 }
 fn two(_input: &[String]) {
@@ -23,6 +33,11 @@ fn two(_input: &[String]) {
 
 fn main() {
     let input = input();
+    assert_eq!(
+        input.len(),
+        1,
+        "Ignoring new lines means the input is one line"
+    );
     one(&input);
     two(&input);
 }
