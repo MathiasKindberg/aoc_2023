42b2bf6cd4da9f616a931d3edd4f68aab51a7ba6
Mathias Kindberg <mathias@kindberg.earth>
2023-12-28 13:45:42 +0100

    day 15 part 2 progress!

--- days/aoc_15/src/main.rs
+++ days/aoc_15/src/main.rs
@@ -1,4 +1,4 @@
-//! Part 1:
+//! Part 1: 515495
 //! Part 2:
 
 use std::io::BufRead;
@@ -24,9 +24,48 @@ fn one(input: &[String]) {
     let sum: u64 = input[0].split(',').map(encode).sum();
     println!("One: {sum} | Elapsed: {:?}", now.elapsed());
 }
-fn two(_input: &[String]) {
+
+#[derive(Debug, Clone)]
+struct Lens<'a> {
+    label: &'a str,
+    focal_length: usize,
+    hash: u64,
+}
+
+fn two(input: &[String]) {
     let now = std::time::Instant::now();
     let sum = 0;
+    let operations: Vec<_> = input[0].split(',').collect();
+
+    // Start with 255 empty boxes to ensure that all boxes we index into
+    // based on the "HASH" exists.
+    let boxes: Vec<Vec<Lens>> = vec![vec![]; 255];
+    println!("{operations:?}");
+    for op in operations {
+        if op.contains('=') {
+            let mut op = op.split('=');
+            let label = op.next().unwrap();
+            let lens = Lens {
+                label,
+                focal_length: op.next().unwrap().parse::<usize>().unwrap(),
+                hash: encode(label),
+            };
+
+            // dbg!(encode(lens.label));
+            println!("{lens:?}");
+            // if let Some(idx) = boxes
+            //     .iter()
+            //     .position(|lens_box| lens_box.label == lens.label)
+            // {
+            // } else {
+            // }
+        } else if op.contains('-') {
+            let label = op.replace('-', "");
+            println!("{label:5} -> --")
+        } else {
+            unreachable!("Unknown symbol in {op}")
+        }
+    }
 
     println!("Two: {sum} | Elapsed: {:?}", now.elapsed());
 }
