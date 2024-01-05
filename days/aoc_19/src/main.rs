69e1499dbed360fd3a1c95507254915fb984db71
Mathias Kindberg <mathias@kindberg.earth>
2024-01-05 12:19:55 +0100

    day 19 part 1

--- days/aoc_19/src/main.rs
+++ days/aoc_19/src/main.rs
@@ -18,14 +18,11 @@ fn input() -> Input {
     let workflows = input
         .next()
         .unwrap()
-        .into_iter()
+        .iter()
         .map(|flow| {
-            let (key, input) = flow.split_once("{").unwrap();
-            let input = input.replace("}", "");
-            (
-                key.to_owned(),
-                input.split(',').map(|step| Step::new(step)).collect(),
-            )
+            let (key, input) = flow.split_once('{').unwrap();
+            let input = input.replace('}', "");
+            (key.to_owned(), input.split(',').map(Step::new).collect())
         })
         .collect();
 
@@ -60,45 +57,55 @@ impl Part {
             s: values[3][2..].parse().unwrap(),
         }
     }
+
+    fn sum(&self) -> usize {
+        self.x + self.m + self.a + self.s
+    }
 }
 
 #[derive(Debug)]
 struct Step {
     // A step may sometimes not have a condition
     condition: Option<Condition>,
-    target: WorkflowResult,
+    target: Target,
 }
 
 impl Step {
     fn new(condition: &str) -> Self {
-        if condition.contains(":") {
-            let (operation, target) = condition.split_once(":").unwrap();
+        if condition.contains(':') {
+            let (operation, target) = condition.split_once(':').unwrap();
             Self {
                 condition: Some(Condition {
                     category: Category::new(&operation[0..1]),
                     operator: Operator::new(&operation[1..2]),
                     value: operation[2..].parse().unwrap(),
                 }),
-                target: WorkflowResult::new(target),
+                target: Target::new(target),
             }
         } else {
             Self {
                 condition: None,
-                target: WorkflowResult::new(condition),
+                target: Target::new(condition),
             }
         }
     }
 
     fn apply(&self, part: &Part) -> WorkflowResult {
         if let Some(condition) = &self.condition {
-            // Todo: apply logic
-            self.target.clone()
+            let applicable = match condition.category {
+                Category::X => condition.operator.compare(part.x, condition.value),
+                Category::M => condition.operator.compare(part.m, condition.value),
+                Category::A => condition.operator.compare(part.a, condition.value),
+                Category::S => condition.operator.compare(part.s, condition.value),
+            };
+
+            if applicable {
+                WorkflowResult::Applicable(self.target.clone())
+            } else {
+                WorkflowResult::NotApplicable
+            }
         } else {
-            assert!(
-                self.target.is_workflow(),
-                "Expected workflow target if no condition"
-            );
-            self.target.clone()
+            WorkflowResult::Applicable(self.target.clone())
         }
     }
 }
@@ -144,17 +151,30 @@ impl Operator {
             other => unreachable!("Unknown operator: {other}"),
         }
     }
+
+    fn compare(&self, a: usize, b: usize) -> bool {
+        match self {
+            Self::Greater => a > b,
+            Self::Less => a < b,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum WorkflowResult {
+    Applicable(Target),
+    NotApplicable,
 }
 
 // Should likely be renamed
 #[derive(Debug, Clone)]
-enum WorkflowResult {
+enum Target {
     Accepted,
     Rejected,
     Workflow(String),
 }
 
-impl WorkflowResult {
+impl Target {
     fn new(target: &str) -> Self {
         match target {
             "A" => Self::Accepted,
@@ -162,41 +182,43 @@ impl WorkflowResult {
             target => Self::Workflow(target.to_owned()),
         }
     }
-
-    fn is_workflow(&self) -> bool {
-        match self {
-            WorkflowResult::Workflow(_) => true,
-            _ => false,
-        }
-    }
 }
 
 fn one(input: Input) {
     let (workflows, parts) = input;
     let now = std::time::Instant::now();
-    let sum = 0;
 
-    for workflow in &workflows {
-        println!("{workflow:?}")
-    }
+    let mut accepted = 0;
+    let mut sum = 0;
 
-    println!("====");
-    let workflow = String::from("in");
-    for part in parts {
+    for part in &parts {
         // Todo, should keep on going until done
-        let flow = workflows.get(&workflow).unwrap();
-        println!("{part:?}");
-        println!("{flow:?}");
+        let mut next_step = Some(String::from("in"));
 
-        for step in workflows.get(&workflow).unwrap() {
-            // Keep on applying steps until we get a result
-            step.apply(&part);
+        while let Some(workflow) = next_step.take() {
+            for step in workflows.get(&workflow).unwrap() {
+                // Keep on applying steps until we get a result and then jump to that.
+                match step.apply(part) {
+                    WorkflowResult::Applicable(result) => {
+                        match result {
+                            Target::Accepted => {
+                                accepted += 1;
+                                sum += part.sum();
+                            }
+                            Target::Rejected => (),
+                            Target::Workflow(new_flow) => next_step = Some(new_flow),
+                        }
+                        break;
+                    }
+                    WorkflowResult::NotApplicable => (),
+                }
+            }
         }
-
-        break;
     }
 
-    println!("One: {sum} | Elapsed: {:?}", now.elapsed());
+    let elapsed = now.elapsed();
+    println!("Accepted: {accepted} | Elapsed: {elapsed:?}",);
+    println!("Sum: {sum} | Elapsed: {elapsed:?}",);
 }
 
 fn two(_input: &[String]) {
