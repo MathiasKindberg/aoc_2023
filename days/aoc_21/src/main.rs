d431525e85af39af8086d7377a383c1f636aef3e
Mathias Kindberg <mathias@kindberg.earth>
2024-01-07 21:44:35 +0100

    update aoc_ex and start working on day 21

---
+++ days/aoc_21/src/main.rs
@@ -0,0 +1,61 @@
+//! Part 1:
+//! Part 2:
+
+use std::io::BufRead;
+
+type Input = Vec<Vec<Tile>>;
+
+#[derive(Debug, Clone, PartialEq, Eq, Copy, Hash)]
+enum Tile {
+    StartingPosition,
+    Rock,
+    GardenPlot,
+}
+
+impl std::fmt::Display for Tile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Tile::GardenPlot => write!(f, "·"), // Switch to interpunct "."
+            Tile::StartingPosition => write!(f, "S"),
+            Tile::Rock => write!(f, "#"),
+        }
+    }
+}
+
+fn one(_input: &Input) {
+    let now = std::time::Instant::now();
+    let sum = 0;
+
+    println!("One: {sum} | Elapsed: {:?}", now.elapsed());
+}
+fn two(_input: &Input) {
+    let now = std::time::Instant::now();
+    let sum = 0;
+
+    println!("Two: {sum} | Elapsed: {:?}", now.elapsed());
+}
+
+fn parse(input: &Vec<String>) -> Input {
+    input
+        .iter()
+        .map(|row| {
+            row.chars()
+                .map(|c| match c {
+                    'S' => Tile::StartingPosition,
+                    '#' => Tile::Rock,
+                    '.' => Tile::GardenPlot,
+                    err => unreachable!("Unknown input: {err:?}"),
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect()
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let input = stdin.lock().lines().map_while(Result::ok).collect();
+
+    let input = parse(&input);
+    one(&input);
+    two(&input);
+}
--- days/aoc_ex/src/main.rs
+++ days/aoc_ex/src/main.rs
@@ -5,11 +5,6 @@ use std::io::BufRead;
 
 type Input = Vec<String>;
 
-fn input() -> Input {
-    let stdin = std::io::stdin();
-    stdin.lock().lines().map_while(Result::ok).collect()
-}
-
 fn one(_input: &Input) {
     let now = std::time::Instant::now();
     let sum = 0;
@@ -23,8 +18,16 @@ fn two(_input: &Input) {
     println!("Two: {sum} | Elapsed: {:?}", now.elapsed());
 }
 
+fn parse(input: &Vec<String>) -> Input {
+    input.iter().map(|row| row.to_owned()).collect()
+}
+
 fn main() {
-    let input = input();
+    // Keep input owned by main function to allow nifty references.
+    let stdin = std::io::stdin();
+    let input: Vec<String> = stdin.lock().lines().map_while(Result::ok).collect();
+    let input = parse(&input);
+
     one(&input);
     two(&input);
 }
